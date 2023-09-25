16c22f7869a3c555ab310e42725344a86c3607e8
Adrian Moore <adrianrob@hotmail.com>
2023-09-24 18:01:48 -0700

    change start image and added federation flag as example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,29 +32,17 @@ export class AppComponent implements AfterViewInit {
     "formatOnPaste": true,
     "formatOnType": true
   };
-  svgcode: string = `
-<svg xmlns="http://www.w3.org/2000/svg" stroke-linejoin="round" viewBox="0 0 100 100">
-  <path d="M50,4L4,50L50,96L96,50Z" stroke="#FE4" stroke-width="3"/>
-  <path d="M50,5L5,50L50,95L95,50Z" stroke="#333" fill="#FE4" stroke-width="3"/>
-  <g transform="scale(0.8) translate(14,30)">
-    <path d="M37,42c-1,0,11-20,13-20c1,0,15,20,13,20h-9c0,8,9,22,12,25l-4,4l-8,-7v13h-10v-35z" stroke="#CA0" fill="#CA0"/>
-    <path d="M35,40c-1,0,11-20,13-20c1,0,15,20,13,20h-9c0,8,9,22,12,25l-4,4l-8,-7v13h-10v-35z" stroke="#333" fill="#555"/>
-  </g>
- <g transform="translate(50,26) scale(0.25)" stroke-width="2">
-   <g fill="none">
-    <ellipse stroke="#469" rx="6" ry="44"/>
-    <ellipse stroke="#ba5" rx="6" ry="44" transform="rotate(-66)"/>
-    <ellipse stroke="#68c" rx="6" ry="44" transform="rotate(66)"/>
-    <circle  stroke="#331" r="44"/>
-   </g>
-   <g fill="#689" stroke="#FE4">
-    <circle fill="#80a3cf" r="13"/>
-    <circle cy="-44" r="9"/>
-    <circle cx="-40" cy="18" r="9"/>
-    <circle cx="40" cy="18" r="9"/>
-   </g>
- </g>
- </svg>  `
+  svgcode: string = `<!-- https://github.com/HatScripts/circle-flags/blob/gh-pages/flags/ca.svg -->
+<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
+	<mask id="a">
+		<circle cx="256" cy="256" r="256" fill="#fff"/>
+	</mask>
+	<g mask="url(#a)">
+		<path fill="#d80027" d="M0 0v512h144l112-64 112 64h144V0H368L256 64 144 0Z"/>
+		<path fill="#eee" d="M144 0h224v512H144Z"/>
+		<path fill="#d80027" d="m301 289 44-22-22-11v-22l-45 22 23-44h-23l-22-34-22 33h-23l23 45-45-22v22l-22 11 45 22-12 23h45v33h22v-33h45z"/>
+	</g>
+</svg>`
 
   @ViewChild('test') test!: ElementRef<HTMLDivElement>;
 
